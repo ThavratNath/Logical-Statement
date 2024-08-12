81f6b29d59e4093f945d5fb6491ceac24dde7ab1
Thavrat Nath <thavratnath@gmail.com>
2024-08-12 12:28:03 +0700

    Add files via upload

---
+++ 4_Control_Statement.py
@@ -0,0 +1,108 @@
+
+'''There are two types of Control Flow:
+1. Condition statement: if, else, & elif
+2. Loop: while & for '''
+
+
+'''I. Condition Statement: '''
+'''
+1. if statement
+2. if elif statement
+3. if else statement'''
+
+'''1. if statement'''
+'''A'''
+# age = 18
+# if age > 17:
+#     print("You can get married and can be my wife.")
+
+'''B'''
+# age = 17 
+# if age < 17: 
+#     print("You can get married.")
+# else: 
+#     print("You cannt get married.")
+
+'''C'''
+# ans = input("Do you love me? Yes/No :")
+# if ans == "Yes" or ans == "yes" or ans == "YES":
+#     print("I love your so much. ")
+# else:
+#     print("I hate you.")
+    
+'''D'''
+
+'''if the user put the ANSWER (No), his computer will be shut down.'''
+
+# import os         # We use this function when we want to shut down the user's computer.
+
+# ans = input("Do you love me? Yes/No :")
+# if ans == "Yes" or ans == "yes" or ans == "YES":
+#     print("I love your so much. ")
+# else:
+#     os.system("shutdown /s /t 0")
+
+
+'''E'''
+
+# a, b, c = int(input("Enter a: ")), int(input("Enter b: ")), int(input("Enter c: "))
+
+# if a > b and a > c:
+#     print("a is the maximum number.")
+# if b > a and b > c:
+#     print("b is the maximun number.")
+# if c > a and c > b:
+#     print("c is the maximun number.")
+    
+# if a == b and a > c:
+#     print("a and b are the maximun number.")
+# if a == b and b < c:
+#     print(" c is the maximun number.")
+# if a == c and c > b:
+#     print(" a and c are the maximun number.")
+# if a == c and a < b:
+#     print("b is the maximun number")
+# if c == b and b > a:
+#     print(" c and b are the maximun number.")
+# if b == c and b < a:
+#     print("b and c are the maximun number.")
+# if a == b and a == c:
+#     print("a, b and c are equal.")
+
+
+'''
+2. if elif statement & 
+3. if else statement'''
+
+'''a. '''
+
+# price =float(input("Enter price: "))
+
+# qty = int(input("Enter Qty: "))
+# total = price * qty 
+
+# if total >=0 and total <10:
+#     discount = 0
+# elif total >= 10 and total < 25:
+#     discount = 5
+# else:
+#     discount = 10
+
+# payment = total - (total * discount/100)
+
+# print(f"Total = {total}$")
+# print(f"After discount {discount}%")
+# print(f"Payment = {payment}$")
+
+'''b'''
+#score = int(input("Enter your score: "))
+
+
+     
+
+
+
+
+
+
+'''3. if else statement'''
